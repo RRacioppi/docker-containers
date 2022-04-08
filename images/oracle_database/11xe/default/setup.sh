d9c72778717b20ec4575f0b6ec38f706a56421df
Renzo Racioppi <rracioppi@imolainformatica.it>
2022-04-08 14:56:34 +0200

    gitignore and setup.sh oracle

--- images/oracle_database/11xe/default/setup.sh
+++ images/oracle_database/11xe/default/setup.sh
@@ -6,4 +6,8 @@ docker cp ./sql oracle:/opt
 docker cp "backup.dmp" "oracle:/opt/orabackup/backup.dmp"
 docker exec -it --user oracle oracle sqlplus system/manager "@/opt/sql/DropUser.sql" "$ORACLE_USER"
 docker exec -it --user oracle oracle sqlplus system/manager "@/opt/sql/CreateUser.sql" "$ORACLE_USER"
+
+## Drakkar Specific
+docker exec -it --user oracle oracle sqlplus system/manager "@/opt/sql/SetupDump.sql"
+
 docker exec -it --user oracle oracle impdp "$ORACLE_USER/$ORACLE_PASSWORD" "directory=exp_dir" "dumpfile=backup.dmp"
